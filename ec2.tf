1122a3209d328b8ff6165d13782b712fa0ee872e
Avinash Yadav <avinash.guruka@gmail.com>
2023-02-28 18:40:50 +0530

    updating tf-module-rabbitmq

---
+++ ec2.tf
@@ -0,0 +1,18 @@
+#resource "aws_instance" "web" {
+#  ami           = data.aws_ami.ami.image_id
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "HelloWorld"
+#  }
+#}
+
+resource "aws_spot_instance_request" "instance" {
+  ami           = data.aws_ami.ami.image_id
+  spot_price    = "0.0031"
+  instance_type = "t3.medium"
+
+  tags = {
+    Name = "CheapWorker"
+  }
+}
