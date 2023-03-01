77b10fde3e5784ebcd8897096584b858a09fc814
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-01 15:50:17 +0530

    updating tf-module-rabbitmq

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "instance" {
   spot_price    = data.aws_ec2_spot_price.spot_price.spot_price
   instance_type = var.INSTANCE_TYPE
   wait_for_fulfillment = true
-  security_group_ids   =  [aws_security_group.sg.id]
+  vpc_security_group_ids =  [aws_security_group.sg.id]
   subnet_id = var.PRIVATE_SUBNET_ID[0]
   iam_instance_profile = aws_iam_instance_profile.profile.name
 
