feb2f695aa57a492608d2b7370ccfd8c4a21c64d
Avinash Yadav <avinash.guruka@gmail.com>
2023-04-18 20:32:51 +0530

    updating tf-module-rabbitmq

--- route53.tf
+++ route53.tf
@@ -2,6 +2,6 @@ resource "aws_route53_record" "rabbitmq" {
   zone_id = var.PRIVATE_HOSTED_ZONE_ID
   name    = "rabbitmq-${var.ENV}.roboshop.internal"
   type    = "A"
-  ttl     = 300
+  ttl     = 30
   records = [aws_spot_instance_request.instance.private_ip]
 }
